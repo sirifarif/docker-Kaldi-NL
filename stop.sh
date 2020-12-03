8f408a7afc2656c6397f521887b98d42cc04deae
Arif Khan <a.khan@let.ru.nl>
2020-12-03 14:38:00 +0100

    Some comments for stop all process

--- stop.sh
+++ stop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#pid=`ps axf | grep worker.py | grep -v grep | awk '{print "echo " $1}' | sh`
+#for x in $pid;do kill -15 ${x};done
+
 #kill worker
 ps axf | grep worker.py | grep -v grep | awk '{print "kill -15 " $1}' | sh
 
